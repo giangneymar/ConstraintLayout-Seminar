416841bcc34f25205c8055c48a8e5a3259e686fe
Giang <dinhbao161087@gmail.com>
2022-06-13 11:41:16 +0700

    add binding

--- app/src/main/java/com/example/constraintlayoutseminar/MainActivity.java
+++ app/src/main/java/com/example/constraintlayoutseminar/MainActivity.java
@@ -2,21 +2,37 @@ package com.example.constraintlayoutseminar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.constraintlayoutseminar.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        onClickG();
     }
 
     /**
      * code cua Dat
      * /
-
-    /*
+     * <p>
+     * /*
      * code cua Giang
      */
+    private void onClickG() {
+        binding.motionScene.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, MotionActivity.class);
+            startActivity(intent);
+        });
+    }
 }
