ed7aa300cd2099a8f573085ce9574d4be99bba78
Giang <dinhbao161087@gmail.com>
2022-06-13 17:56:06 +0700

    code motion_scene

--- app/src/main/java/com/example/constraintlayoutseminar/MotionActivity.java
+++ app/src/main/java/com/example/constraintlayoutseminar/MotionActivity.java
@@ -3,12 +3,27 @@ package com.example.constraintlayoutseminar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.constraintlayoutseminar.databinding.ActivityMotionBinding;
 
 public class MotionActivity extends AppCompatActivity {
+
+    private ActivityMotionBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_motion);
+        binding = ActivityMotionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        onClick();
     }
+
+
+    private void onClick() {
+        binding.buttonPress.setOnClickListener(view -> binding.motionSceneContainer.transitionToEnd());
+
+        binding.buttonReset.setOnClickListener(view -> binding.motionSceneContainer.transitionToStart());
+    }
+
 }
